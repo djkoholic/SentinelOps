7578afa853a7ca054ad973387eb6e7a166197e26
djkoholic <jaivardhanbiyani@gmail.com>
2026-06-18 12:30:58 +0000

    small bugfix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from agents import parse_time_range, run_investigator
 from data_collector import collect_data
 
-if __name__ == 'main':
+if __name__ == '__main__':
     query = input("What is your query?\n")
     print(f"[DEBUG] User query: {query}")
     # User enters: Some of the requests between 11:30 to 12:00 PM Failed. Investigate what happened.
@@ -15,4 +15,8 @@ if __name__ == 'main':
     #context = build_context(all_data)
     answer = run_investigator(query, all_data)
     print("[DEBUG] Investigation complete")
-    print(answer)
+    print()
+    for key, value in answer.items():
+        print(f"{key.upper()}:")
+        print(value)
+        print()
